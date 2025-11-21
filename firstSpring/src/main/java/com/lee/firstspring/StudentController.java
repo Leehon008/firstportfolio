0ca8058f6e8fe8ddeef96ecb5e68f6fa9183e7ca
Lewis <honye.lewism@gmail.com>
2025-11-20 22:27:04 -0800

    Initial commit for backend code

---
+++ firstSpring/src/main/java/com/lee/firstspring/StudentController.java
@@ -0,0 +1,34 @@
+package com.lee.firstspring;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    @GetMapping("/")
+    public List<String> getStudents() {
+        return List.of("Student1", "Student2", "Student3");
+    }
+
+    @GetMapping("/{id}")
+    public String getStudent(@PathVariable String id) {
+        return "Student " + id;
+    }
+
+    @PostMapping("/create")
+    public String createStudent() {
+        return "Created Student";
+    }
+
+    @PutMapping("/{id}")
+    public String putStudentById(@PathVariable String id){
+        return "Student " + id + " updated";
+    }
+
+    @PatchMapping("/{id}")
+    public String patchStudentById(@PathVariable String id){
+        return "Student " + id + " patched";
+    }
+}
