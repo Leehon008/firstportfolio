a1d753dde430e58743b659b51ef57c548c2326ea
Lewis <honye.lewism@gmail.com>
2025-11-22 16:42:38 -0800

    fix: footer to dynamic

--- src/app/custom_components/footer/footer.ts
+++ src/app/custom_components/footer/footer.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+username: string = 'Mr Lee';
+githubUrl: string = "https://github.com/Leehon008";
+linkedInUrl: string = "https://www.linkedin.com/in/lewis-honye-008/";
 }
